2ae8fceabd80acea9e4de5da35ac737673db6b6d
svalvastic <assogames2@gmail.com>
2022-11-03 15:23:12 +0100

    ✨ feat(migration): train migration

--- database/migrations/2022_11_03_140616_create_trains_table.php
+++ database/migrations/2022_11_03_140616_create_trains_table.php
@@ -15,6 +15,25 @@ class CreateTrainsTable extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+//             Azienda
+// Stazione di partenza
+// Stazione di arrivo
+// Orario di partenza
+// Orario di arrivo
+// Codice Treno
+// Numero Carrozze
+// In orario
+// Cancellato
+
+            $table->string('company');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->string('departure_time');
+            $table->string('arrival_time');
+            $table->string('train_code');
+            $table->string('carriages');
+            $table->string('on_time');
+            $table->string('cancelled');
             $table->timestamps();
         });
     }
