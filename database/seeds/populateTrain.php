1cbc9ec3055fdb109629362ee3bad9d4fd41cc0e
svalvastic <assogames2@gmail.com>
2022-11-03 16:27:38 +0100

    ✨ feat(seed):

--- database/seeds/populateTrain.php
+++ database/seeds/populateTrain.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
-class populateTrain extends Seeder
+use App\Train;
+use Faker\Generator as Faker;
+class PopulateTrain extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +12,35 @@ class populateTrain extends Seeder
      */
     public function run()
     {
-        //
+        $train = new Train();
+        $train->company = 'Trenitalia';
+        $train->departure_station = 'Roma Termini';
+        $train->arrival_station = 'Milano Centrale';
+        $train->departure_time = '10:00';
+        $train->arrival_time = '12:00';
+        $train->train_code = 'T123';
+        $train->carriages = '10';
+        $train->on_time = '1';
+        $train->cancelled = '0';
+        $train->save();
+
+    }
+}
+
+class FakerTrains extends Seeder {
+    public function run(Faker $faker) {
+        for($i=0; $i< 100;$i++){
+            $train = new Train();
+            $train->company = $faker->company;
+            $train->departure_station = $faker->city;
+            $train->arrival_station = $faker->city;
+            $train->departure_time = $faker->time;
+            $train->arrival_time = $faker->time;
+            $train->train_code = $faker->randomNumber(3);
+            $train->carriages = $faker->randomNumber(2);
+            $train->on_time = $faker->boolean;
+            $train->cancelled = $faker->boolean;
+            $train->save();
+        }
     }
 }
